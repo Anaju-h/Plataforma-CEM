d347d341bd7b733f9f0929a176476fdb0baea20c
Anaju-h <anajuh.1997@gmail.com>
2026-09-14 09:29:02 -0300

    chore: remove estrutura legada do frontend Next

--- src/app/configurador/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Configurator } from "@/components/configurator/Configurator";
-
-export default function ConfiguradorPage() {
-  return (
-    <main className="min-h-screen bg-[#eef3f6]">
-      <Configurator />
-    </main>
-  );
-}
